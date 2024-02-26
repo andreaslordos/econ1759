af5497887e9bab35473ee868786f7142db857fd7
Andreas Lordos <andreasglordos@gmail.com>
2024-02-26 12:56:10 -0500

    added cdo def prob

--- experiment3.py
+++ experiment3.py
@@ -3,22 +3,25 @@ import pandas as pd
 import streamlit as st
 
 # Load the updated data from the CSV file
-df = pd.read_csv('cdo2_default_probabilities_updated.csv')
+df = pd.read_csv('cdo2_default_probabilities_updated_with_cdo.csv')
 
 # Initialize the figure
 fig = go.Figure()
 
 default_tranche = 10
 
-# Add the initial trace using the default tranche data
-fig.add_trace(go.Scatter(x=df['ind_default_prob'], y=df[f'tranche_{default_tranche}'], mode='lines'))
+# Add the initial trace using the default tranche data for CDO^2
+fig.add_trace(go.Scatter(x=df['ind_default_prob'], y=df[f'cdo2_tranche_{default_tranche}'], mode='lines', name='CDO^2 Default Probability'))
+
+# Add a second trace for the CDO default probability (initially set for the default tranche)
+fig.add_trace(go.Scatter(x=df['ind_default_prob'], y=df[f'cdo_tranche_{default_tranche}'], mode='lines', name='CDO Default Probability'))
 
 # Update layout with the slider for tranche adjustments
 fig.update_layout(
-    title="Probability of CDO^2 Default based on Individual Default Probability",
+    title="Probability of CDO^2 and CDO Default based on Individual Default Probability",
     xaxis_title="Individual Default Probability",
     xaxis_range=[0, 1],  # Set the x-axis range to 0 to 1
-    yaxis_title="Probability of CDO^2 Default",
+    yaxis_title="Probability of Default",
     sliders=[{
         'pad': {'t': 50},
         'len': 0.9,
@@ -33,7 +36,7 @@ fig.update_layout(
         'steps': [{
             'label': f'{i}',
             'method': 'restyle',
-            'args': [{'y': [df[f'tranche_{i}']]}]
+            'args': [{'y': [df[f'cdo2_tranche_{i}'], df[f'cdo_tranche_{i}']]}]
         } for i in range(101)]
     }]
 )
--- generate_data.py
+++ generate_data.py
@@ -2,9 +2,16 @@ import numpy as np
 import pandas as pd
 from scipy.stats import binom
 
+# Function to calculate the probability of a CDO default
+def cdo_default_probability(ind_default_prob, tranche):
+    # Probability that the number of defaults exceeds the tranche threshold
+    return 1 - binom.cdf(tranche - 1, 100, ind_default_prob)
+
 # Function to calculate the probability of a CDO^2 default
 def cdo2_default_probability(ind_default_prob, tranche):
-    prob_cdo_default = 1 - binom.cdf(tranche - 1, 100, ind_default_prob)
+    # First calculate the probability of a CDO default
+    prob_cdo_default = cdo_default_probability(ind_default_prob, tranche)
+    # Then calculate the probability of a CDO^2 default based on the CDO default probability
     prob_cdo2_default = 1 - binom.cdf(tranche - 1, 100, prob_cdo_default)
     return prob_cdo2_default
 
@@ -15,9 +22,15 @@ tranches = range(101)  # Range of tranches
 # Prepare a DataFrame to hold the data
 data = {'ind_default_prob': ind_default_probs}
 for tranche in tranches:
-    data[f'tranche_{tranche}'] = [cdo2_default_probability(ind_default_prob, tranche) for ind_default_prob in ind_default_probs]
+    # Calculate CDO^2 default probabilities for each tranche
+    cdo2_probs = [cdo2_default_probability(ind_default_prob, tranche) for ind_default_prob in ind_default_probs]
+    data[f'cdo2_tranche_{tranche}'] = cdo2_probs
+    
+    # Calculate CDO default probabilities for each tranche
+    cdo_probs = [cdo_default_probability(ind_default_prob, tranche) for ind_default_prob in ind_default_probs]
+    data[f'cdo_tranche_{tranche}'] = cdo_probs
 
 df = pd.DataFrame(data)
 
 # Save the DataFrame to a file
-df.to_csv('cdo2_default_probabilities_updated.csv', index=False)
+df.to_csv('cdo2_default_probabilities_updated_with_cdo.csv', index=False)
